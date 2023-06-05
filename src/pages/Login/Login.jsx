2316dcaeba330c8e53089a9f99ef2a71023df1ec
ForhadRoben <i.rforhad@gmail.com>
2023-06-05 12:06:50 +0600

    firebase and authprovider set up

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+
+const Login = () => {
+    // const captchaRef = useRef(null);
+    const [disable, setDisable] = useState(true)
+
+    useEffect(() => {
+        loadCaptchaEnginge(6)
+    }, [])
+
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+    const handleValidateCaptcha = (e) => {
+        const user_captcha_value = e.target.value;
+        console.log(user_captcha_value);
+        // console.log(user_captcha_value);
+        if (validateCaptcha(user_captcha_value)) {
+            setDisable(false)
+        } else {
+            setDisable(true)
+        }
+    }
+
+    //use useRef
+    // const handleValidateCaptcha = () => {
+    //     const user_captcha_value = captchaRef.current.value;
+    //     console.log(user_captcha_value);
+    //     // console.log(user_captcha_value);
+    //     if (validateCaptcha(user_captcha_value)) {
+    //         setDisable(false)
+    //     } else {
+    //         setDisable(true)
+    //     }
+    // }
+    return (
+        <div>
+            <Helmet>
+                <title>Restau Boss | Login</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex flex-col md:flex-row">
+                    <div className="text-center lg:text-left w-full md:w-1/4">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card w-full md:w-3/4  shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <LoadCanvasTemplate />
+                                </label>
+                                <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type captcha above" className="input input-bordered" />
+
+
+
+                                {/* <input ref={captchaRef}  type="text" name="captcha" placeholder="type captcha above" className="input input-bordered" /> */}
+                                {/* <button onClick={handleValidateCaptcha} className="btn btn-ghost btn-xs" >Validate</button> */}
+
+                            </div>
+                            <div className="form-control mt-6">
+                                <input disabled={disable} className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
